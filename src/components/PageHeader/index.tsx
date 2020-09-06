226921267171d2af4d9dda71f0f2f029b55b3178
GuilhermeKiyoshi <kiyoshi.guilherme@gmail.com>
2020-09-05 23:14:07 -0300

    Estilização da pagina de cadastro dos professores

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -5,8 +5,10 @@ import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
 import './styles.css';
+
 interface PageHeaderProps {
     title: string;
+    description?: string;
 }
 
 const PageHeader: React.FunctionComponent<PageHeaderProps> = (props) => {
@@ -21,6 +23,8 @@ const PageHeader: React.FunctionComponent<PageHeaderProps> = (props) => {
 
             <div className="header-content">
                 <strong>{props.title}</strong>
+                { props.description && <p>{props.description}</p> }
+
                 {props.children}
             </div>
             
